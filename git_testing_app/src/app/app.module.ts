b77e339719684d5b0b6594e754e0feb873ce7fcc
tirtheshdas <tirtheshdas58@gmail.com>
2024-04-12 14:28:28 +0530

    added app module

--- git_testing_app/src/app/app.module.ts
+++ git_testing_app/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderFooterBodyTest1Test2Test3Component } from './header-footer-body-test1-test2-test3/header-footer-body-test1-test2-test3.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { BodyComponent } from './body/body.component';
+import { Test1Component } from './test1/test1.component';
+import { Test2Component } from './test2/test2.component';
+import { Test3Component } from './test3/test3.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderFooterBodyTest1Test2Test3Component,
+    HeaderComponent,
+    FooterComponent,
+    BodyComponent,
+    Test1Component,
+    Test2Component,
+    Test3Component
   ],
   imports: [
     BrowserModule,
